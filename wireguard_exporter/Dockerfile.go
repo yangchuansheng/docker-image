70ff35e497d2e25d6d9f7f85a5198a74612eadc2
Ryan <yangchuansheng33@gmail.com>
2020-07-17 15:25:24 +0800

    Update Dockerfile.go

--- wireguard_exporter/Dockerfile.go
+++ wireguard_exporter/Dockerfile.go
@@ -1,8 +1,13 @@
 FROM golang AS build
+
+LABEL description="Docker container for building prometheus exporter for wireguard."
+LABEL maintainer="Ryan Yang <yangchuansheng33@gmail.com>"
+
 WORKDIR /src
 RUN git clone https://github.com/mdlayher/wireguard_exporter; \
     cd wireguard_exporter/cmd/wireguard_exporter/; \
     go build .
+
 FROM busybox:glibc
 COPY --from=build /src/wireguard_exporter/cmd/wireguard_exporter/wireguard_exporter .
 CMD ["./wireguard_exporter"]
