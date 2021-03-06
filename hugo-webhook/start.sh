05da5846d16a4452b3cf6d77716bfcdfb114c16c
yangchuansheng <4L^Ei@5iZZY#YfV4478&>
2021-03-06 23:27:00 +0800

    🐛 fix bug

--- hugo-webhook/start.sh
+++ hugo-webhook/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sed -i -e "s/\$GITHUB_USERNAME/$GITHUB_USERNAME/" ~/bin/docker-image/hugo-webhook/hugo-update.yaml
-sed -i -e "s/\$GITHUB_PASSWD/$GITHUB_PASSWD/" ~/bin/docker-image/hugo-webhook/hugo-update.yaml
+sed -i -e "s/\$GITHUB_USERNAME/$GITHUB_USERNAME/" hugo-update.yaml
+sed -i -e "s/\$GITHUB_PASSWD/$GITHUB_PASSWD/" hugo-update.yaml
 
 kubectl get job hugo-update && kubectl delete job hugo-update
 kubectl apply -f hugo-update.yaml
