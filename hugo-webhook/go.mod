b3aa8d39b81c52934f7048d7c0681e85bf1ad677
yangchuansheng <4L^Ei@5iZZY#YfV4478&>
2021-03-08 00:43:05 +0800

    🏗 change nodejs to go

---
+++ hugo-webhook/go.mod
@@ -0,0 +1,8 @@
+module hugo-webhook
+
+go 1.15
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/go-playground/webhooks.v5 v5.17.0
+)
---
+++ hugo-webhook/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"net/http"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
+
+const (
+	path = "/"
+)
+
+func main() {
+	hook, _ := github.New(github.Options.Secret(os.Getenv("WEBHOOK_SECRET")))
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.PushEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				fmt.Printf("got err, %+v\n", err)
+			}
+		}
+		switch payload.(type) {
+
+		case github.PushPayload:
+			cmd := exec.Command("sh", "-c", "./start.sh")
+			if output, err := cmd.CombinedOutput(); err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Println(string(output))
+			}
+		}
+	})
+	http.ListenAndServe(":80", nil)
+}
