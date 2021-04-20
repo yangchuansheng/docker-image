3bcf852f8b0356e0ff33914b1a1a98b38244be7b
RyanYang <yangchuansheng33@gmail.com>
2021-04-20 17:32:58 +0800

    Update entrypoint.sh

--- chia/miner/entrypoint.sh
+++ chia/miner/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sed -i "/apiKey/c apiKey: $(APIKEY)" config.yaml
+sed -i "/apiKey/c apiKey: $APIKEY" config.yaml
 sed -i "/minerName/c minerName: $HOSTNAME" config.yaml
 
 ./hpool-miner-chia
