78479dd94fa47f5d4ac0d063dd4876b5f2a52189
yangchuansheng <yangchuansheng33@gmail.com>
2021-08-02 07:35:08 +0000

    update lotus-farcaster

--- lotus-farcaster/docker_run_script.sh
+++ lotus-farcaster/docker_run_script.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # Try to run lotus farcaster executing at the defined FREQUENCY. If farcaster take more time, the script will wait 10s before the next execution
 
-sed -Ei "/^MINER_URL =/c MINER_URL = \"$MINER_URL\"" /usr/local/bin/lotus-exporter-farcaster.py
-sed -Ei "/^MINER_TOKEN =/c MINER_TOKEN = \"$MINER_TOKEN\"" /usr/local/bin/lotus-exporter-farcaster.py
-sed -Ei "/^DAEMON_URL =/c DAEMON_URL = \"$DAEMON_URL\"" /usr/local/bin/lotus-exporter-farcaster.py
-sed -Ei "/^DAEMON_TOKEN =/c DAEMON_TOKEN = \"$DAEMON_TOKEN\"" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#http://127.0.0.1:2345/rpc/v0#$MINER_URL#" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#MINER_TOKEN = \"\"#MINER_TOKEN = \"$MINER_TOKEN\"#" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#http://127.0.0.1:1234/rpc/v0#$DAEMON_URL#" /usr/local/bin/lotus-exporter-farcaster.py
 
 while true; do
     BEG=$(date +%s)
